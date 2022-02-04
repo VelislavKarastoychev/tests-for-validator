876726762981070de57751193f225f39addd5ab4
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-05 00:39:40 +0200

    select all the tests

--- Tests/index.js
+++ Tests/index.js
@@ -8,13 +8,37 @@
  * @author Velislav S. Karastoychev
  * @email euriklis@hotmail.com
  */
-import {test_integer} from './testInteger'
-import {testIsSameWithAny} from './testIsSameWithAny'
-import {testInterface2} from './testInterface2'
-import {testForAll} from './testForAll'
+import {test_integer} from './testInteger.js';
+import {testIsBoolean} from './testIsBoolean.js';
+import {testIsSameWithAny} from './testIsSameWithAny.js';
+import {testInterface2} from './testInterface2.js';
+import {testForAll} from './testForAll.js';
+import {testIsNumber} from './testIsNumber.js';
+import {testIsFloat} from './testIsFloat.js';
+import {testIsUndefined} from './testIsUndefined.js';
+import {testIsEmpty} from './testIsEmpty.js';
+import {testIsString} from './testIsString.js';
+import {testIsArray} from './testIsArray.js';
+import {testIsNumberArray} from './testIsNumberArray.js';
+import {testIsObject} from './testIsObject.js';
+import {testIsStringArray} from './testIsStringArray.js';
+import {testIsFunction} from './testIsFunction.js';
+import {testForAny} from "./testForAny.js";
 export const tests = [
     test_integer,
+    testIsBoolean,
     testIsSameWithAny,
     testInterface2,
     testForAll,
+    testIsNumber,
+    testIsFloat,
+    testIsUndefined,
+    testIsEmpty,
+    testIsString,
+    testIsArray,
+    testIsNumberArray,
+    testIsObject,
+    testIsStringArray,
+    testIsFunction,
+    testForAny,
 ]
