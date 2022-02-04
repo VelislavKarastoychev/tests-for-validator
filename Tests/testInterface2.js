1696fde792023bdcd7bc06db58dad40080059665
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-05 00:41:36 +0200

    test the interface2 method of the validator package.

--- Tests/testInterface2.js
+++ Tests/testInterface2.js
@@ -1,18 +1,18 @@
 'use strict'
 import validator from '@euriklis/validator'
-import { success_message } from '../Messages'
-import { error_message } from '../Messages'
+import { success_message } from '../Messages/index.js';
+import { error_message } from '../Messages/index.js';
 const Interface2 = (value) => {
     return new validator(value).interface2({
         'first name': fn => fn.is_string(),
         'last name': ln => ln.is_string(),
-        age: age => age.is_integer().and().is_in_range(0, 200),
-        email: email => email.is_string()
+        age: age => age.is_integer().And.is_in_range(0, 200),
+        email: email => email.isString
     }).answer
 }
 export const testInterface2 = new Promise((resolve, reject) => {
     new validator(Interface2([2, 'a', 14, () => { }, 32.2]))
-        .is_same(false).and().bind(
+        .is_same(false).And.bind(
             new validator(Interface2(235)).is_same(false)
                 .on(true, () => success_message('interface2 first test is successful', 'yellow').log())
                 .on(false, () => error_message('interface2 first test is wrong...').log())
@@ -25,8 +25,8 @@ export const testInterface2 = new Promise((resolve, reject) => {
             new validator(Interface2({
                 'first name': 'John',
                 'last name': 'Jones',
+                age: 28,
                 email: 'johnJones@correctEmail.com',
-                age: 28
             })).is_same(true)
                 .on(false, () => error_message('interface2 third test is wrong...', 'red').log())
                 .on(true, () => success_message('interface2 third test is successful', 'yellow').log())
