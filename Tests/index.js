8bc0ff799c204d1ea0e0affcd0d0eb43d92d87bb
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-09 23:26:08 +0300

    Collection of all the tests of the @euriklis/validator library.

--- Tests/index.js
+++ Tests/index.js
@@ -8,28 +8,30 @@
  * @author Velislav S. Karastoychev
  * @email euriklis@hotmail.com
  */
-import {test_integer} from './testInteger.js';
-import {testIsBoolean} from './testIsBoolean.js';
-import {testIsSameWithAny} from './testIsSameWithAny.js';
-import {testInterface2} from './testInterface2.js';
-import {testForAll} from './testForAll.js';
-import {testIsNumber} from './testIsNumber.js';
-import {testIsFloat} from './testIsFloat.js';
-import {testIsUndefined} from './testIsUndefined.js';
-import {testIsEmpty} from './testIsEmpty.js';
-import {testIsString} from './testIsString.js';
-import {testIsArray} from './testIsArray.js';
-import {testIsNumberArray} from './testIsNumberArray.js';
-import {testIsObject} from './testIsObject.js';
-import {testIsStringArray} from './testIsStringArray.js';
-import {testIsFunction} from './testIsFunction.js';
-import {testForAny} from "./testForAny.js";
+import { test_integer } from './testInteger.js';
+import { testIsBoolean } from './testIsBoolean.js';
+import { testIsSameWithAny } from './testIsSameWithAny.js';
+import { testInterface2 } from './testInterface2.js';
+import { testForAll } from './testForAll.js';
+import { testForAll2 } from './testForAll2.js';
+import { testIsNumber } from './testIsNumber.js';
+import { testIsFloat } from './testIsFloat.js';
+import { testIsUndefined } from './testIsUndefined.js';
+import { testIsEmpty } from './testIsEmpty.js';
+import { testIsString } from './testIsString.js';
+import { testIsArray } from './testIsArray.js';
+import { testIsNumberArray } from './testIsNumberArray.js';
+import { testIsObject } from './testIsObject.js';
+import { testIsStringArray } from './testIsStringArray.js';
+import { testIsFunction } from './testIsFunction.js';
+import { testForAny } from "./testForAny.js";
 export const tests = [
     test_integer,
     testIsBoolean,
     testIsSameWithAny,
     testInterface2,
     testForAll,
+    testForAll2,
     testIsNumber,
     testIsFloat,
     testIsUndefined,
