a720568be890b179e1fa329d1f0ae4aa1cf180e4
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-10 17:21:51 +0300

    Tests initialilzation.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict'
-import {header, error_message, success_message} from './Messages/index.js';
-import {tests} from './Tests/index.js';
-let printer = header('@euriklis/validator testing message')
+import { header, error_message, success_message } from './Messages/index.js';
+import { tests } from './Tests/index.js';
+import validator from '@euriklis/validator';
+let printer = header(`@euriklis/validator , version ${validator.version} testing message`)
 Promise.all(tests)
-.then((tests) => {
-    tests.forEach(test => {
-         printer = printer
-        .append(success_message(`Successfully tested ${test} method\n`).text)
+    .then((tests) => {
+        tests.forEach(test => {
+            printer = printer
+                .append(success_message(`Successfully tested ${test} method\n`).text)
+        })
+        printer.log()
+    }).catch((err) => {
+        printer.append(error_message(err).text).log()
     })
-    printer.log()
-}).catch((err) => {
-    printer.append(error_message(err).text).log()
-})
