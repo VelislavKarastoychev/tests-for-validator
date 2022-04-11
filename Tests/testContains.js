674518b7ee8110a70bfac82be9f7c237303f87c8
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-11 19:06:19 +0300

    tests for the contains() method of the @euriklis validator.

--- Tests/testContains.js
+++ Tests/testContains.js
@@ -2,17 +2,37 @@
 import * as print from '../Messages/index.js';
 import validator from '@euriklis/validator';
 const testContains = new Promise((resolve, reject) => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split();
-    const string_array_from_numbers = Array.from({ length: 20 }).map((element, index) => element = (index + 1) + '');
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+    const string_array_from_numbers = Array.from({ length: 10 }).map((element, index) => element = index + '');
     const string_array = ['b', 'a', 'n', 'a', 'n', 'n', 'a'];
-    new validator(alphabet).contains(string_array_from_numbers)
-        .on(true, () => print.success_message("Successfully executed first test of the contains() method.").log())
+    const dictionary = [
+        'alphabet',
+        'almost',
+        'Bar',
+        'Chart',
+        'Dictionary',
+        'Penetration',
+        'Probably',
+        'Suitable',
+    ];
+    const words = [
+        'alphabet',
+        'Dictionary',
+        'Suitable'
+    ];
+    new validator(string_array_from_numbers).contains(alphabet)
+        .on(true, () => print.success_message("Successfully executed first test of the contains() method.", 'yellow').log())
         .on(false, () => print.error_message("Error in the first test of the contains method.").log())
         .And.bind(
-            new validator(alphabet).contains(string_array)
-            .on(true, () => print.success_message("Successfully executed second test of the contains() method.").log())
+            new validator(string_array).contains(alphabet)
+            .on(true, () => print.success_message("Successfully executed second test of the contains() method.", 'yellow').log())
             .on(false, () => print.error_message('Error in the second test of the contains() method.').log())
-        ).on(true, () => resolve('contains()'))
+        ).And.bind(
+            new validator(words).contains(dictionary)
+            .on(true, () => print.success_message('Successfully executed third test of the contains() method.', 'yellow').log())
+            .on(false, () => print.error_message('Error in the third test of the contains() method.').log())
+        )
+        .on(true, () => resolve('contains()'))
         .on(false, () => reject('contains()'))
 });
 export { testContains };
