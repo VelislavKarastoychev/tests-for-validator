27bf0d68b11560ede3d3ae28829edfdfacab1384
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-11 19:21:30 +0300

    Initialization of the tests...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-'use strict'
+#!/usr/bin/env node
+
+'use strict';
 import { header, error_message, success_message } from './Messages/index.js';
 import { tests } from './Tests/index.js';
 import validator from '@euriklis/validator';
