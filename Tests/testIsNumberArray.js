914620a81e34c9e438cdc6ceb35a1a35225ec1b1
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-20 23:17:21 +0200

    Checks if the method works correct.

--- Tests/testIsNumberArray.js
+++ Tests/testIsNumberArray.js
@@ -4,6 +4,7 @@ import * as print from '../Messages/index.js';
 const method = "is_number_array() or isNumberArray";
 const testIsNumberArray = new Promise((resolve, reject) => {
     new validator([1, 3, 4, 5]).is_number_array()
+        .And.bind(new validator([6, 7, 8]).is_number_array())
         .on(true, () => {
             print.success_message("is_number_array() first test successful", "yellow").log();
         }).on(false, () => {
@@ -18,4 +19,4 @@ const testIsNumberArray = new Promise((resolve, reject) => {
         ).on(true, () => resolve(method))
         .on(false, () => reject(method));
 });
-export {testIsNumberArray};
+export { testIsNumberArray };
